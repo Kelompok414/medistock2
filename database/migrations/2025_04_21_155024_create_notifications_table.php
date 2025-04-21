41c2c4c581d848b4cc3401c8e8909cfa377e448b
cherryjayhawk <muhbintangr19@gmail.com>
2025-04-22 00:01:06 +0700

    fix foreign keu on user id

--- database/migrations/2025_04_21_155024_create_notifications_table.php
+++ database/migrations/2025_04_21_155024_create_notifications_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('notifications', function (Blueprint $table) {
             $table->id();
-            $table->uuid('user_id');
+            $table->unsignedBigInteger('user_id');
             $table->string('message');
             $table->boolean('is_read')->default(false);
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
