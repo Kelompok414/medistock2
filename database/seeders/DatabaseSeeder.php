d78d19015e9ac79f2976fe46166acc634eaf5299
cherryjayhawk <muhbintangr19@gmail.com>
2025-04-22 01:20:14 +0700

    fix seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,29 +13,19 @@ class DatabaseSeeder extends Seeder
 {
     public function run()
     {
-        // 1) Categories
         Category::factory()->count(5)->create();
-
-        // 2) Medicines (each will get a random category via the factory)
-        Medicine::factory()->count(20)->create();
-
-        // 3) Batches (each will get a random medicine via the factory)
-        Batch::factory()->count(50)->create();
-
-        // 4) Transactions + SaleItems
+        Medicine::factory()->count(10)->create();
+        Batch::factory()->count(10)->create();
         Transaction::factory()
-            ->count(30)
+            ->count(10)
             ->create()
-            ->each(function ($tx) {
-                // grab all existing batch IDs
+            ->each(function (Transaction $tx) {
                 $batchIds = Batch::pluck('id')->toArray();
-                // choose 1–5 items per transaction
-                $itemCount = rand(1, 5);
+                $items = rand(1, 5);
 
-                for ($i = 0; $i < $itemCount; $i++) {
+                for ($i = 0; $i < $items; $i++) {
                     Saleitem::factory()->create([
                         'transaction_id' => $tx->id,
-                        // pick a random existing batch
                         'batch_id'       => $batchIds[array_rand($batchIds)],
                     ]);
                 }
