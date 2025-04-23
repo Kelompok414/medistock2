6fd51b56ad11f56d755d7b972425716bdedec464
cherryjayhawk <muhbintangr19@gmail.com>
2025-04-23 21:33:54 +0700

    add relations and casts transaction date

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -14,4 +14,18 @@ class Transaction extends Model
     protected $keyType = 'string';
     protected $fillable = ['id', 'user_id', 'total_price', 'transaction_date'];
     public $timestamps = true;
+
+    protected $casts = [
+        'transaction_date' => 'datetime',
+    ];
+    
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function saleitems()
+    {
+        return $this->hasMany(SaleItem::class);
+    }
 }
